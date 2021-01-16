ce61b47454b347921ef341c6332819a1739c3946
cjlworld <cjlworld@users.noreply.github.com>
2021-01-16 16:03:08 +0800

    2020.1.16

---
+++ PI acw#3125/data.cpp
@@ -0,0 +1,27 @@
+#include<set>
+#include<map>
+#include<queue>
+#include<stack>
+#include<ctime>
+#include<cmath>
+#include<bitset>
+#include<vector>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<complex>
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+
+int main()
+{
+	srand((unsigned) time(0));
+	cout<<rand()+1<<" "<<rand()+1<<" "<<rand()<<endl;
+	puts("0 0 0");
+	return 0;
+}
+
